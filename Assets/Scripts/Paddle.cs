cee963c6425f5b137ae955cfc969b07871c5e783
zekk01 <57352910+zekk01@users.noreply.github.com>
2021-01-20 23:20:57 +0200

    added plugins and touch input for paddle movement

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -11,10 +11,13 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
 
-        transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
-
+            transform.Translate(touchDeltaPosition.x * speed, 0, 0);
+        }
         if (transform.position.x < leftscreenedge)
                 {
             transform.position = new Vector2(leftscreenedge, transform.position.y);
